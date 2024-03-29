d5a1d2bab1cca49d92fc8d66121feecc3e74c05d
CharLee Racine <charleeracine@yahoo.com>
2024-03-29 13:15:34 -0600

    finished two_sum

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,5 +1,22 @@
 class Array
 
+    def my_uniq
+        new_array = []
+        self.each do |ele|
+            new_array << ele if !new_array.include?(ele)
+        end
+        new_array
+    end
+
+    def two_sum
+        array = []
+        self.each_with_index do |ele, idx|
+            self.each_with_index do |ele_2, idx_2|
+                array << [idx, idx_2] if ele + ele_2 == 0 && idx < idx_2
+            end
+        end
+        array
+    end
 
 
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,14 +2,32 @@ require "tdd"
 require "rspec"
 
 
-# describe "Array"
+describe "Array" do
+    subject(:array) {[1,2,1,3,3]}
+
     describe "#my_uniq" do
         it "should accept an array as argument" do
-            expect{[1, 2, 1, 3, 3].uniq}.not_to raise_error()
+            expect{array.my_uniq}.not_to raise_error()
         end
 
+        it "should remove duplicates from an array" do
+            expect(array.my_uniq).to eq([1,2,3])
+        end
 
-
-
+        # it "tests stuff" do
+        #     expect(true).to eq(false)
+        # end
     end
-# end
+
+    describe "#two_sum" do
+        it "should accept an array as argument" do
+            expect{array.two_sum}.not_to raise_error()
+        end
+
+        it "finds all pairs of positions where the elements at those positions sum to zero" do
+            new_array = [-1, 0, 2, -2, 1]
+            expect(new_array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+
+end
